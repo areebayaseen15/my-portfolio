e26ff85a8be7113531040185590ff83d79e4e5a3
Areeba Yaseen <147875287+areebayaseen15@users.noreply.github.com>
2025-06-22 03:09:02 +0500

    Update agentProjectsData.ts

--- src/app/Components/Projects/agentProjectsData.ts
+++ src/app/Components/Projects/agentProjectsData.ts
@@ -1,21 +1,21 @@
-
-
-
-export const Projects= [
-  
-        {
-    title: "Simple AI Agent with model Selection",
-    description:  "A basic AI agent that selects the best model for a given task using OpenAI's Agent SDK, demonstrating how to leverage different models for optimal performance.",
+export const Projects = [
+  {
+    title: "Simple AI Agent with Model Selection",
+    description:
+      "A basic AI agent that selects the best model for a given task using OpenAI's Agent SDK, demonstrating how to leverage different models for optimal performance.",
     video: "/Assest/Vedios/ai-model-selection.mp4",
     link: "#",
-    github: "https://github.com/areebayaseen15/Quarter_4_Agentic_Ai-with-sir-Aneeq/blob/main/task1/Practical%20Work/Repo_02_OpenRouter/app.py",
+    github:
+      "https://github.com/areebayaseen15/Quarter_4_Agentic_Ai-with-sir-Aneeq/blob/main/task1/Practical%20Work/Repo_02_OpenRouter/app.py",
+    tech: ["python", "openai" , "approuter" , "streamlit"],
   },
-      {
+  {
     title: "LinkedIn Agent",
-    description: "An intelligent automation tool built with OpenAI Agent SDK that posts updates on LinkedIn autonomously — perfect for managing your professional presence without manual effort.",
+    description:
+      "An intelligent automation tool built with OpenAI Agent SDK that posts updates on LinkedIn autonomously — perfect for managing your professional presence without manual effort.",
     video: "/Assest/Vedios/agent.mp4",
     link: "#",
     github: "https://github.com/your-linkedin-agent-repo",
+    tech: ["python", "openai" , "chainlit" , "gemini"],
   },
-
-]
+];
